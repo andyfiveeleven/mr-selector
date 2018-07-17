8dca71035b1b57e7bc7375acd329a5087c3772f5
andyfiveeleven <fouracesguitar@gmail.com>
2018-07-17 14:48:14 -0700

    modal modularity

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -6,9 +6,9 @@ class Modal extends React.Component {
       <section className='modal'>
         <div className='pop-up'>
           <button onClick={this.props.close}>X</button>
-          <button onClick={this.props.reset}>Reset Mr Selector</button>
-          <h3>Your Selection Is!</h3>
-          <p>{this.props.choice}</p>
+          <button onClick={this.props.reset}>{this.props.button2}</button>
+          <h3>{this.props.header}</h3>
+          <p>{this.props.flavor}</p>
         </div>
       </section>
     )
